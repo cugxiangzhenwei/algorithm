3ece24dff3c8bd0f8f8deeea07dcbe08ecd5b2a3
xiangzhenwei <xiangzhenwei@xiangzhenweideMacBook-Pro.local>
2017-08-29 15:01:31 +0800

    sort tree remove node algorithm

--- sort_tree.h
+++ sort_tree.h
@@ -26,6 +26,26 @@ void print_data(T & data)
 	printf(sz_format,data);
 }
 template<typename T>
+void breadth_print(struct node<T> * root)
+{
+	printf("breadth loop result:\n");
+	if(!root) return;
+
+	queue<struct node<T> *> qu;
+	qu.push(root);
+	while(!qu.empty())
+	{
+		struct node<T> * t = qu.front();
+		qu.pop();
+		print_data(t->data);
+		if(t->left)
+			qu.push(t->left);
+		if(t->right)
+			qu.push(t->right);
+	}
+	printf("\n");
+}
+template<typename T>
 void mid_print(struct node<T> *t)
 {
 	if(t==NULL) return;
@@ -92,6 +112,7 @@ node<T> * insert(struct node<T> * root,T data)
 template<typename  T >
 void free_tree(struct node<T> * &root)
 {
+	if(root==NULL) return;
 	std::stack<struct node<T> *> s;
 	s.push(root);
 	while(!s.empty())
@@ -112,6 +133,8 @@ void free_tree(struct node<T> * &root)
 template<typename T >
 int count(struct node<T> * root)
 {
+	if(root==NULL) return 0;
+
 	int icount = 0;
 	queue< struct node<T> *> s;
 	s.push(root);
@@ -126,4 +149,104 @@ int count(struct node<T> * root)
 			s.push(t->right);
 	}
 	return icount;
+}
+template<typename T>
+node<T> * find(struct node<T> * root, const T & data)
+{
+	struct node<T> * p = root;
+	while(p)
+	{
+		if(p->data == data)
+			break;
+		else if(p->data > data)
+			p = p->left;
+		else if(p->data < data)
+			p = p->right;	
+	}
+	return p;
+}
+template<typename T>
+bool exist(struct node<T> * root,const T & data)
+{
+	return find(root,data)!=NULL;
+}
+template<typename T>
+node<T> * find_min(struct node<T> * root,struct node<T> * & min_parent)
+{
+	struct node<T> * p = root;
+	while(p)
+	{
+		if(p->left ==NULL)
+			break;
+		else
+		{
+			min_parent = p;
+			p = p->left;
+		}
+	}		
+	return p;
+}
+template<typename T>
+node<T> * find_max(struct node<T> * root,struct node<T> * & max_parent)
+{
+	struct node<T> * p = root;
+	while(p)
+	{
+		if(p->right ==NULL)
+			break;
+		else
+		{
+			max_parent = p;
+			p = p->right;
+		}
+	}		
+	return p;
+}
+template<typename T>
+node<T> * remove(struct node<T> * root,const T & data)
+{
+	if(data < root->data)
+		root->left = remove(root->left,data);	
+	else if(data > root->data)
+		root->right = remove(root->right,data);	
+	else
+	{
+		if(root->left!=NULL && root->right!=NULL)
+		{
+			struct node<T> * min_parent = root;
+			struct node<T> * tmin = find_min(root->right,min_parent);
+			root->data = tmin->data;
+			struct node<T> * p = remove(tmin,tmin->data);
+			if(p==NULL)
+			{
+				min_parent->right = NULL;
+				if(root !=min_parent)
+					min_parent->left = NULL;
+			}
+			else if(min_parent == p)
+				p->right = p;
+			else
+				min_parent->left = p;
+			return root;
+
+		}
+		else if(root->left!=NULL && root->right==NULL)
+		{
+			struct node<T> * p = root->left;
+			delete root;
+			return p;
+		}
+		else if(root->right!=NULL && root->left == NULL)
+		{
+			struct node<T> * p = root->right;
+			delete root;
+			return p;
+		}
+		else
+		{
+			free_tree(root);
+			return NULL;
+		}
+	}
+	return root;
 }
--- sort_tree_test.cpp
+++ sort_tree_test.cpp
@@ -23,8 +23,99 @@ bool test_case1()
 	free_tree(root);
 	return true;
 }
+void test_case2()
+{
+	int arry[] = {6,2,8,1,5,3,4};
+	int icount = sizeof(arry)/sizeof(int);
+	struct node<int> * root = NULL;
+	root = insert(root,arry[0]);
+	for(int i=1;i<icount;i++)
+		insert(root,arry[i]);
+
+	breadth_print(root);
+	mid_print(root);
+
+	remove(root,4);
+	breadth_print(root);
+	mid_print(root);
+
+	remove(root,2);
+	breadth_print(root);
+	mid_print(root);
+	free_tree(root);
+}
+void test_case3()
+{
+	int arry[] = {6,2,8,1,5,3,4};
+	int icount = sizeof(arry)/sizeof(int);
+	struct node<int> * root = NULL;
+	root = insert(root,arry[0]);
+	for(int i=1;i<icount;i++)
+		insert(root,arry[i]);
+
+	breadth_print(root);
+	mid_print(root);
+	while(count(root)>0)
+	{
+		printf("input data will removed :\n");
+		int data = 0;
+		scanf("%d",&data);
+	    root = remove(root,data);
+		printf("after remove data:%d\n",data);
+		breadth_print(root);
+		mid_print(root);
+	}
+}
+void test_case4()
+{
+	int arry[] = {3,1,5,4};
+	int icount = sizeof(arry)/sizeof(int);
+	struct node<int> * root = NULL;
+	root = insert(root,arry[0]);
+	for(int i=1;i<icount;i++)
+		insert(root,arry[i]);
+
+	breadth_print(root);
+	mid_print(root);
+	while(count(root)>0)
+	{
+		printf("input data will removed :\n");
+		int data = 0;
+		scanf("%d",&data);
+	    root = remove(root,data);
+		printf("after remove data:%d\n",data);
+		breadth_print(root);
+		mid_print(root);
+	}
+}
+void test_case5()
+{
+	int arry[] = {4,1,5};
+	int icount = sizeof(arry)/sizeof(int);
+	struct node<int> * root = NULL;
+	root = insert(root,arry[0]);
+	for(int i=1;i<icount;i++)
+		insert(root,arry[i]);
+
+	breadth_print(root);
+	mid_print(root);
+	while(count(root)>0)
+	{
+		printf("input data will removed :\n");
+		int data = 0;
+		scanf("%d",&data);
+	    root = remove(root,data);
+		printf("after remove data:%d\n",data);
+		breadth_print(root);
+		mid_print(root);
+	}
+}
 int main(int,char*[])
 {
+	test_case5();
+	test_case4();
+	test_case3();
+	test_case2();
 	bool bcontinue = true;
 	while(bcontinue)
 	{
